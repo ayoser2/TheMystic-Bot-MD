214e2b382d41b3012c96152d7ae3040a0d78b13f
ayoser2 <107592051+ayoser2@users.noreply.github.com>
2022-08-28 13:43:23 +0530

    ᴠᴇɴᴏᴍ ʏᴛᴠ ᴠ2

--- plugins/downloader-ytv.2.js
+++ plugins/downloader-ytv.2.js
@@ -7,7 +7,7 @@ if (!args || !args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃
 conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '𝚁𝙴𝙿𝚁𝙾𝙳𝚄𝙲𝚃𝙾𝚁 𝙳𝙴 𝚅𝙸𝙳𝙴𝙾 𝚅𝟸',
-body: 'ʙʏ ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ',         
+body: 'ʙʏ ᴛʜᴇ ᴠᴇɴᴏᴍ﹣ ʙᴏᴛ ᴍᴅ',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
 sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
 let chat = global.db.data.chats[m.chat]
